818375227a6587a817600e2f6479459f99581355
noname <9223773@mail.ru>
2022-01-31 15:41:17 +0300

    join table for task added

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
@@ -44,6 +45,7 @@ public class Task {
     private User executor;
     @ManyToMany
     @Nullable
+    @JoinTable(name = "task_labels")
     private List<Label> labels;
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
